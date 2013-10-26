483ea4a9a7074e358e61b97fe9d1207f1e92c501
bbaclig <bbaclig@ucsd.edu>
2013-10-26 02:43:34 -0700

    map

--- map_init.py
+++ map_init.py
@@ -14,11 +14,6 @@ map_lo = [[0 for j in range(width)] for i in range(height)]
 n = 0
 
 # store tile data into map_lo
-#for i in range(height):
- # for j in range(width):
-  #  map_lo[i][j] = data['layers'][0]['data'][n]
-   # n += 1
-
 for row in map_lo:
   for tile in range(len(row)):
      row[tile] = data['layers'][0]['data'][n]
