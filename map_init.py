800b97c9095990386d19543b9cd69c354647272b
bbaclig <bbaclig@ucsd.edu>
2013-10-26 02:40:39 -0700

    first commit map

--- map_init.py
+++ map_init.py
@@ -9,28 +9,24 @@ data = json.loads(json_input)
 
 height = data['layers'][0]['height']
 width= data['layers'][0]['width']
-row_lo = [0 for i in range(width)]
-map_lo = [[0 for j in range(len(row_lo))] for i in range(height)]
+map_lo = [[0 for j in range(width)] for i in range(height)]
 
 n = 0
-m = 0
 
-print len(map_lo)
+# store tile data into map_lo
+#for i in range(height):
+ # for j in range(width):
+  #  map_lo[i][j] = data['layers'][0]['data'][n]
+   # n += 1
 
-for a in range(0, height):
-  # print a, "HERE"
-  for b in range(0, width):
-    row_lo[b] = data['layers'][0]['data'][n]
-    #print row_lo[b]
-    n += 1
-  map_lo[a] = row_lo
-  print map_lo[a]
+for row in map_lo:
+  for tile in range(len(row)):
+     row[tile] = data['layers'][0]['data'][n]
+     n += 1
 
-#for a in map_lo:
-#  for b in map_lo[b]:
-#    print b
-#    print map_lo[a][b],
-#  print "\n"
+# prints out the map layout
+for row in map_lo:
+  for tile in row:
+    print tile, " ",
+  print "\n"
 
-# for x, row in range(0, 3):
-#  print "blah"
